b5cd8c542b713b0e65337c24fced24f06f3045ae
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-10-10 12:22:49 +0200

    refactor(front-end): cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script>
-export default {
-	name: "App",
-};
-</script>
+<style lang="scss">
+	@import './assets/css/style.scss';
+</style>
 
 <template>
-	<div>Hello, World!</div>
+	<div id="nav">
+		<router-link to="/">Home</router-link> |
+		<router-link to="/about">About</router-link>
+	</div>
+	<router-view />
 </template>
