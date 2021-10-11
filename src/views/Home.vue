61e002e89ce079178c0d4a6b308f32c3354021b9
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-10-11 16:48:22 +0200

    feat(front-end): add delete button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,18 +10,36 @@ export default {
 			forum: [],
 		};
 	},
-	mounted() {
-		var ctx = this;
-		document.title = "Groupomania | Accueil";
-		ctx.axios
-			.get(ctx.$endPoint + "/thread/", {
+	methods: {
+		deleteThread(id) {
+			var ctx = this;
+			ctx.axios.delete(ctx.$endPoint + "/thread/" + id, {
 				headers: {
 					authorization: "Bearer: " + localStorage.getItem("token"),
 				},
-			})
-			.then(function (res) {
-				ctx.forum = res.data;
+			}).then(function()
+			{
+				ctx.loadAllThread();
 			});
+		},
+		loadAllThread() {
+			var ctx = this;
+			ctx.axios
+				.get(ctx.$endPoint + "/thread/", {
+					headers: {
+						authorization:
+							"Bearer: " + localStorage.getItem("token"),
+					},
+				})
+				.then(function (res) {
+					ctx.forum = res.data;
+				});
+		},
+	},
+	mounted() {
+		var ctx = this;
+		document.title = "Groupomania | Accueil";
+		ctx.loadAllThread();
 	},
 };
 </script>
@@ -45,6 +63,9 @@ export default {
 				<router-link :to="{ name: 'Thread', params: { id: value.id } }"
 					><div class="more">Cliquer ici pour la voir la suite</div>
 				</router-link>
+				<div class="delete" v-if="value.creator === $getInfo().userId || $getInfo().status === 1" v-on:click="deleteThread(value.id)">
+					Supprimer le thread
+				</div>
 			</div>
 		</section>
 	</div>
