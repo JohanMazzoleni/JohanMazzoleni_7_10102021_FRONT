11045c5e1de88618e808005db406c351afc08e8b
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-10-11 18:29:05 +0200

    fix(front-end): fix html problems

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,14 +13,16 @@ export default {
 	methods: {
 		deleteThread(id) {
 			var ctx = this;
-			ctx.axios.delete(ctx.$endPoint + "/thread/" + id, {
-				headers: {
-					authorization: "Bearer: " + localStorage.getItem("token"),
-				},
-			}).then(function()
-			{
-				ctx.loadAllThread();
-			});
+			ctx.axios
+				.delete(ctx.$endPoint + "/thread/" + id, {
+					headers: {
+						authorization:
+							"Bearer: " + localStorage.getItem("token"),
+					},
+				})
+				.then(function () {
+					ctx.loadAllThread();
+				});
 		},
 		loadAllThread() {
 			var ctx = this;
@@ -60,11 +62,23 @@ export default {
 				<div class="message">
 					{{ value.message }}
 				</div>
-				<router-link :to="{ name: 'Thread', params: { id: value.id } }"
-					><div class="more">Cliquer ici pour la voir la suite</div>
-				</router-link>
-				<div class="delete" v-if="value.creator === $getInfo().userId || $getInfo().status === 1" v-on:click="deleteThread(value.id)">
-					Supprimer le thread
+				<div class="thread-option">
+					<router-link
+						:to="{ name: 'Thread', params: { id: value.id } }"
+						><div class="more">
+							Cliquer ici pour la voir la suite
+						</div>
+					</router-link>
+					<div
+						class="delete"
+						v-if="
+							value.creator === $getInfo().userId ||
+							$getInfo().status === 1
+						"
+						v-on:click="deleteThread(value.id)"
+					>
+						Supprimer le thread
+					</div>
 				</div>
 			</div>
 		</div>
