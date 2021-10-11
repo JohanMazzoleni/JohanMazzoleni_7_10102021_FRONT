0179245ce65b8c2b8932573dbd426d18109381cb
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-10-11 16:48:05 +0200

    feat(front-end): add getInfo

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import jwt from 'jsonwebtoken';
 
 const app = createApp(App);
 
@@ -11,13 +12,21 @@ app.config.globalProperties.$endPoint = "http://localhost:3000";
 axios.interceptors.response.use(function (response) {
 	return response;
 }, function (error) {
-	if (error.response.data && error.response.data.status == "bad_token")
+	if (error && error.response && error.response.data && error.response.data.status == "bad_token")
     {
         localStorage.removeItem("token");
         router.push({name: "Login"});
     }
     return Promise.reject(error);
 });
+
+var getInfo = function()
+{
+    var user = localStorage.getItem("token");
+    return jwt.decode(user);
+};
+
+app.config.globalProperties.$getInfo = getInfo;
 
 app.use(VueAxios, axios);
 app.use(router);
