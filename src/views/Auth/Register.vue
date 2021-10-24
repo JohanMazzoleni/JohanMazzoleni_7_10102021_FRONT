fa38fd519aa39725bfdafd65b656e89b78bacebb
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-10-24 16:18:51 +0200

    feat(front-end): password validator

--- src/views/Auth/Register.vue
+++ src/views/Auth/Register.vue
@@ -1,4 +1,6 @@
 <script>
+import passwordValidator from "password-validator";
+
 export default {
 	mounted() {
 		document.title = "Groupomania | S'inscrire";
@@ -8,12 +10,61 @@ export default {
 			email: "test@example.com",
 			username: "JohnDoe",
 			password: "12345",
+			repassword: "12345",
+			errorMessage: null,
 		};
 	},
 	methods: {
 		register(e) {
 			var ctx = this;
 			e.preventDefault();
+
+			if (ctx.password !== ctx.repassword) {
+				alert("Les mots de passe ne sont pas identiques");
+				return;
+			}
+
+			var schema = new passwordValidator();
+
+			schema
+				.is()
+				.min(1)
+				.is()
+				.max(80)
+				.has()
+				.uppercase()
+				.has()
+				.lowercase()
+				.has()
+				.digits(1)
+				.has()
+				.not()
+				.spaces()
+				.is()
+				.not()
+				.oneOf(["Passw0rd", "Password123"]);
+
+			let isValidPassword = schema.validate(ctx.password, {
+				list: true,
+			});
+
+			let errorTemplate = {
+				min: "Le mot de passe n'est pas assez long.",
+				uppercase: "Votre mot de passe doit contenir des majuscules.",
+				digits: "Le mot de passe doit contenir des chiffres.",
+				letters: "Le mot de passe doit contenir des lettres",
+				lowercase:
+					"Le mot de passe doit contenir des lettres minuscule.",
+				symbols: "Le mot de passe doit contenir des symboles",
+			};
+
+			if (isValidPassword[0]) {
+				ctx.errorMessage = errorTemplate[isValidPassword[0]];
+				return;
+			} else {
+				ctx.errorMessage = null;
+			}
+
 			ctx.axios
 				.post(this.$endPoint + "/auth/register", {
 					email: ctx.email,
@@ -25,21 +76,20 @@ export default {
 						localStorage.setItem("token", res.data.token);
 						alert("Votre inscription a bien été effectuer.");
 						ctx.$router.push({
-							name: "Home"
-						})
+							name: "Home",
+						});
 					}
 				})
 				.catch(function (err) {
 					let errorMessage = "Une erreur est survenue.";
-					if (err.response.data.err && err.response.status == "409")
-					{
-						if (err.response.data.err == "email_already_used")
-						{
+					if (err.response.data.err && err.response.status == "409") {
+						if (err.response.data.err == "email_already_used") {
 							errorMessage = "Votre email est déjà utilisé.";
-						}
-						else if (err.response.data.err == "username_already_used")
-						{
-							errorMessage = "Votre nom d'utilisateur est déjà utilisé.";
+						} else if (
+							err.response.data.err == "username_already_used"
+						) {
+							errorMessage =
+								"Votre nom d'utilisateur est déjà utilisé.";
 						}
 					}
 					alert(errorMessage);
@@ -81,6 +131,19 @@ export default {
 						id="password"
 						type="password"
 						v-model="password"
+						maxlength="80"
+						required
+					/>
+					<span v-if="errorMessage" class="errors">{{
+						errorMessage
+					}}</span>
+				</div>
+				<div class="col-1">
+					<label for="password">Répeter mot de passe: </label>
+					<input
+						id="repassword"
+						type="password"
+						v-model="repassword"
 						maxlength="80"
 						required
 					/>
